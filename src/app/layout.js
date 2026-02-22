672bbbb23e0f1872c3954d3553e68536459760e0
Ramesh beniwal <walferlab@gmail.com>
2026-02-22 10:55:54 +0530

    commit#9 removing extra details

--- src/app/layout.js
+++ src/app/layout.js
@@ -93,20 +93,6 @@ export default function RootLayout({ children }) {
         {googleSiteVerification ? (
           <meta name="google-site-verification" content={googleSiteVerification} />
         ) : null}
-        <!-- Taboola Pixel Code -->
-<script type='text/javascript'>
-  window._tfa = window._tfa || [];
-  window._tfa.push({notify: 'event', name: 'page_view', id: 1997926});
-  !function (t, f, a, x) {
-         if (!document.getElementById(x)) {
-            t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
-         }
-  }(document.createElement('script'),
-  document.getElementsByTagName('script')[0],
-  '//cdn.taboola.com/libtrc/unip/1997926/tfa.js',
-  'tb_tfa_script');
-</script>
-<!-- End of Taboola Pixel Code -->
       </head>
       <body className={`${bodyFont.variable} ${displayFont.variable}`}>
         <JsonLdScript data={globalSchemas} id="global-schema" />
